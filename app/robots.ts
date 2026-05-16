e76a57e8fe2bad9a1f0ac499636081bef8ebc709
Kris Shuman <kris@krisshuman.com>
2026-05-16 19:16:27 -0400

    Fix robots for OG crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,21 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Twitterbot",
+        allow: "/",
+      },
+    ],
     sitemap: "https://krisshuman.com/sitemap.xml",
   };
 }
